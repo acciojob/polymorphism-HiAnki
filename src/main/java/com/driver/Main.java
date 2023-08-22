ee66676bb727d5caf421329e709e1e3014de6c7c
Ankita Pathak <iamankipathak@gmail.com>
2023-08-22 09:33:25 +0530

    commit 0

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,36 +1,29 @@
 package com.driver;
 
 public class Main {
-    public Main() {
 
-    }
     public static void main(String[] args) {
-        // Task 2: Creating an object of the Product class
         Product p = new Product();
-        p.product(1,2);
-        p.product(1,2,3);
-        p.product(2.00,3.00);
-
-        // Now you can work with the 'p' object as needed
+        p.product(1, 2);
+        p.product(1, 2, 3);
+        p.product(2.0, 3.0);
     }
-     static class Product{
-        Product() {
 
-        }
+    public static class Product {
 
         public int product(int x, int y) {
-            System.out.println("first product");
-            return x+y;
+//            System.out.println("first product");
+            return x * y;
         }
+
         public int product(int x, int y, int z) {
-            System.out.println("second product");
-            return x+y+z;
+//            System.out.println("second product");
+            return x * y * z;
         }
+
         public double product(double x, double y) {
-            System.out.println("third product");
-            return x+y;
+//            System.out.println("third product");
+            return x * y;
         }
     }
-
-
 }
